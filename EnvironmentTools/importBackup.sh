c840d9c67165522e4e158fe300c8a6d00da697ea
LytvyniukDima <dima.lytvyniuk@gmail.com>
2019-06-20 04:46:01 +0300

    Add backup data

---
+++ EnvironmentTools/importBackup.sh
@@ -0,0 +1,3 @@
+mongoimport --db EnglishLearning_Statistic --collection CompletedEnglishTask --drop CompletedEnglishTask.json
+
+mongoimport --db EnglishLearning_Statistic --collection CompletedMultimedia --drop CompletedMultimedia.json
---
+++ EnvironmentTools/makeBackup.sh
@@ -0,0 +1,3 @@
+mongoexport --db EnglishLearning_Statistic --collection CompletedEnglishTask -o CompletedEnglishTask.backup.json --pretty
+
+mongoexport --db EnglishLearning_Statistic --collection CompletedMultimedia -o CompletedMultimedia.backup.json --pretty
